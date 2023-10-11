e7a3c3916807b65f1dbb01a99013d5076e3cf2be
pratik9765 <pratikbokde9765@gmail.com>
2023-10-11 12:22:00 +0530

    done

---
+++ String/basics.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class basics{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // char arr[] = {'a','b','c','d','e','f'};
+        // String str = "abcd";
+        // String str2 = new String("xyz");
+
+        // Strings are immutable
+
+        // System.out.println(str);
+        // System.out.println(str2);
+
+        // String name;
+        // System.out.print("Enter Name : ");
+        // name = sc.nextLine();
+        // System.out.println(name.length());
+
+        // int arr2[] = {1,2,3,4,5};
+        // System.out.println(arr2.length);
+        
+        String str1 = "Pratik";
+        String str2 = "Bokde";
+        String fullName = str1 + " " + str2;
+
+         System.out.println(fullName);
+
+    }
+}
